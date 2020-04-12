29a702a47b45636b231287e4d094ad59c4ead916
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-12 19:09:37 +0700

    Update  jenosize map.

---
+++ pages/Jenosize/map.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const JenosizeMapPage = () => {
+  const title = "Jenosize Map"
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <h1>{title}</h1>
+    </>
+  )
+}
+
+export default JenosizeMapPage
