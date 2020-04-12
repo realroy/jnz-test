04ba917e8edfba7230d37ef5a447c96b22bc168d
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-12 20:11:11 +0700

    Styling Navbar.

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,7 +1,29 @@
+import NextLink from "next/link";
 import styles from "./Navbar.module.css";
 
 export const Navbar = () => (
   <header>
-    <nav>Navbar</nav>
+    <nav className={styles.container}>
+      <div className={styles["brand-logo"]}>LOGO</div>
+      <NextLink href="/">
+        <a className={styles["brand-text"]}>website</a>
+      </NextLink>
+      <div className={styles["search-container"]}>
+        <form>
+          <label htmlFor="search">Search</label>
+          <div className={styles["search__input-container"]}>
+            <input
+              className={styles["search__input"]}
+              id="search"
+              type="text"
+              placeholder="Search"
+            />
+            <span className={styles["search__icon"]} type="submit">
+              &#8981;
+            </span>
+          </div>
+        </form>
+      </div>
+    </nav>
   </header>
 );
