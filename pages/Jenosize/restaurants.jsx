0494de35e3bb18ac9e4f37cff84d9e03cf7d4e78
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-12 19:13:29 +0700

    Edit restaurant page.

--- pages/Jenosize/restaurants.jsx
+++ pages/Jenosize/restaurants.jsx
@@ -1,1 +1,15 @@
+import Head from 'next/head'
 
+const JenosizeRestaurantPage = () => {
+  const title = "Restaurants Search"
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <h1>{title}</h1>
+    </>
+  )
+}
+
+export default JenosizeRestaurantPage
